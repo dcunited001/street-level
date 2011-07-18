ae04082df02d7912b324e92b348d5e4c18092922
David Conner <dconner.pro@gmail.com>
2011-07-18 02:41:23 -0400

    added style to site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_page_name
+
+  def set_page_name
+    @page_name = request.request_uri.split('/').last.to_s
+  end
 end
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def active_nav(page_name, link_name)
+    link_reg = Regexp.new(link_name, Regexp::IGNORECASE)
+    ((page_name == '' and link_name == 'home') or page_name =~ link_reg) ? 'active' : ''
+  end
 end
